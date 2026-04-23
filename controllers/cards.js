5e02695e66914166a8f77d99e5cf8058b200a27d
guyera <thegraygrimm@graymansurvivaltech.com>
2026-04-23 19:41:06 -0400

    added the delete and update routes for the cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -9,18 +9,15 @@ router.post("/", verifyToken, async (req, res) => {
       cardName: req.body.cardName,
       setName: req.body.setName,
     });
-
     if (existingCard) {
       existingCard.quantity += 1;
       await exisitingCard.save();
       return res.status(200).json(existingCard);
     }
-
     const newCard = await Card.create({
       ...req.body,
       quantity: 1,
     });
-
     res.status(201).json(newCard);
   } catch (err) {
     res.status(500).json({ err: err.message });
@@ -33,6 +30,60 @@ router.get("/", verifyToken, async (req, res) => {
     res.status(200).json(foundCards);
   } catch (err) {
     res.status(500).json({ err: err.message });
+  }
+});
+
+router.get("/:cardId", verifyToken, async (req, res) => {
+  try {
+    const foundCard = await Card.findById(req.params.cardId);
+    if (!foundCard) {
+      res.status(404);
+      throw new Error("Card not Found");
+    }
+    res.status(200).json(foundCard);
+  } catch (err) {
+    if (res.statusCode === 404) {
+      res.json({ err: err.message });
+    } else {
+      res.status(500).json({ err: err.message });
+    }
+  }
+});
+
+router.put("/:cardId", verifyToken, async (req, res) => {
+  try {
+    const updatedCard = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      req.body,
+      { returnDocument: "after" },
+    );
+    if (!updatedCard) {
+      res.status(404);
+      throw new Error("Card not Found");
+    }
+  } catch (err) {
+    if (res.statusCode === 404) {
+      res.json({ err: err.message });
+    } else {
+      res.status(500).json({ err: err.message });
+    }
+  }
+});
+
+router.delete("/:cardId", verifyToken, async (req, res) => {
+  try {
+    const removedCard = await Card.findByIdAndDelete(req.params.cardId);
+    if (!removedCard) {
+      res.status(404);
+      throw new Error("Card not Found");
+    }
+    res.status(200).json(removedCard);
+  } catch (err) {
+    if (res.statusCode === 404) {
+      res.json({ err: err.message });
+    } else {
+      res.status(500).json({ err: err.message });
+    }
   }
 });
 module.exports = router;
--- controllers/users.js
+++ controllers/users.js
@@ -16,8 +16,7 @@ router.get('/', verifyToken, async (req, res) => {
 
 router.get('/:userId', verifyToken, async (req, res) => {
     try {
-        // If the user is looking for the details of another user, block the request
-        // Send a 403 status code to indicate that the user is unauthorized
+        
         if (req.user._id !== req.params.userId) {
             return res.status(403).json({ err: 'Unauthorized' })
         };
